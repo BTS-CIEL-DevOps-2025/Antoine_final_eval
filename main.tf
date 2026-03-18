d708642b3b552c9dd2afd988d3c3541b41e18418
AntoineCollotSE <antoine.collot@ecole-se.com>
2026-03-18 11:04:49 +0100

    Mise en place de terraform

---
+++ main.tf
@@ -0,0 +1,43 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "Antoine_final_eval_rg"
+  location = "France Central"
+}
+
+
+resource "azurerm_storage_account" "static_site" {
+  name                     = "maxencestaticsite"
+  resource_group_name      = "Antoine_final_eval_rg"
+  location                 = "France Central"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "error.html"
+  }
+}
+
+resource "azurerm_storage_blob" "index" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.static_site.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "index.html"
+  content_type           = "text/html"
+}
+
+
+resource "azurerm_storage_blob" "error" {
+  name                   = "error.html"
+  storage_account_name   = azurerm_storage_account.static_site.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "error.html"
+  content_type           = "text/html"
+}
+
+
+output "static_website_url" {
+  description = "URL of the static website"
+  value       = azurerm_storage_account.static_site.primary_web_endpoint
+}
