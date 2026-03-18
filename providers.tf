c2db6c0fd3133f24f966e2ee41e642031c155da8
AntoineCollotSE <antoine.collot@ecole-se.com>
2026-03-18 11:46:17 +0100

    Changement du storage account name et du container name

--- providers.tf
+++ providers.tf
@@ -17,8 +17,8 @@ terraform {
     tenant_id            = "18057b23-dbd3-4057-9f2f-58efbd9345a8"
     subscription_id      = "f6f9cbca-a3b0-420f-9354-c55f2d575fca"
     resource_group_name  = "ressources_partagees"
-    storage_account_name = "Antoinefinalevalsa"
-    container_name       = "common"
+    storage_account_name = "btsciel2finaleval"
+    container_name       = "Antoine"
     key                  = "terraform.tfstate"
   }
 }
